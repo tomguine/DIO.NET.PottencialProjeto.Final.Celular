f86905606392f2e6c7b7a14ce783c4505e56e85c
Antonio Guimarães Neto <tomguine@gmail.com>
2022-11-02 08:54:19 -0300

    DIO .NET - BootCamp Pottencial - Projeto Final: Smartphone

--- Models/Smartphone.cs
+++ Models/Smartphone.cs
@@ -2,14 +2,39 @@ namespace DesafioPOO.Models
 {
     public abstract class Smartphone
     {
-        public string Numero { get; set; }
-        // TODO: Implementar as propriedades faltantes de acordo com o diagrama
-
-        public Smartphone(string numero)
+        public Smartphone(string tipo, string numero, string modelo, string imei, int memoria) // ctor base
         {
             Numero = numero;
-            // TODO: Passar os parâmetros do construtor para as propriedades
+            // TODO: Passar os parâmetros do construtor para as propriedades - 0k
+            Modelo = modelo;
+            IMEI = imei;
+            Memoria = memoria;
+            Tipo = tipo;
         }
+
+
+        public string Numero { get; set; }
+        // TODO: Implementar as propriedades faltantes de acordo com o diagrama - ok
+
+        private string Modelo { get; set; }
+
+        private string IMEI { get; set; }
+
+        private int Memoria { get; set; }
+        
+        private string Tipo { get; set; }
+
+
+        public void Apresentar()
+        {
+            Console.WriteLine("------------------------");
+            Console.WriteLine(Tipo);
+            Console.WriteLine(Numero);
+            Console.WriteLine(Modelo);
+            Console.WriteLine(IMEI);
+            Console.WriteLine(Memoria + " gb");
+            Console.WriteLine("------------------------");
+        }   
 
         public void Ligar()
         {
--- Program.cs
+++ Program.cs
@@ -1,3 +1,16 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone i1 = new Iphone("(22)2773-4215", "14 Pro", "12345678", 128);
+i1.InstalarAplicativo("DIO.me");
+i1.Ligar();
+i1.ReceberLigacao();
+i1.Apresentar();
+
+
+
+Nokia n1 = new Nokia("(22)99973-4251", "Tijolão", "0987654321", 256);
+n1.InstalarAplicativo("Jogo da cobrinha");
+n1.Ligar();
+n1.ReceberLigacao();
+n1.Apresentar();
